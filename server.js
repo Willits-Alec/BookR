81380171aabac35d3b685ca1441d18299de174f1
tborjal <bor22014@byui.edu>
2024-07-13 23:57:20 +0800

    updates

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
